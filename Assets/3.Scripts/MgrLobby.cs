45fd0f64fc8bc3181783e71d38cabb2b2fa492af
Dontcryjunsu <bassoff@naver.com>
2019-02-07 12:17:18 +0900

    make : DownBtn(#2)

--- Assets/3.Scripts/MgrLobby.cs
+++ Assets/3.Scripts/MgrLobby.cs
@@ -8,10 +8,14 @@ public class MgrLobby : MonoBehaviour
     bool isPan_statusD = false;
     public GameObject Fade;
 
-
-    public GameObject MixturePan;
+    bool isQuest = false;
+    bool isItem = false;
+    bool isCollection = false;
     bool isMixture = false;
-
+    public GameObject QuestPan;
+    public GameObject ItemPan;
+    public GameObject CollectionPan;
+    public GameObject MixturePan;
 
     void Start()
     {
@@ -33,17 +37,80 @@ public class MgrLobby : MonoBehaviour
         }
     }
 
+    public void ClickQuestBtn()
+    {
+        if(!isQuest)
+        {
+            AllBtnDown();
+            QuestPan.SetActive(true);
+            isQuest = true;
+            isItem = false;
+            isCollection = false;
+            isMixture = false;
+        }
+        else if(isQuest)
+        {
+            QuestPan.SetActive(false);
+            isQuest = false;
+        }
+    }
+    public void ClickItemBtn()
+    {
+        if (!isItem)
+        {
+            AllBtnDown();
+            ItemPan.SetActive(true);
+            isQuest = false;
+            isItem = true;
+            isCollection = false;
+            isMixture = false;
+        }
+        else if (isItem)
+        {
+            ItemPan.SetActive(false);
+            isItem = false;
+        }
+    }
+    public void ClickCollectionBtn()
+    {
+        if (!isCollection)
+        {
+            AllBtnDown();
+            CollectionPan.SetActive(true);
+            isQuest = false;
+            isItem = false;
+            isCollection = true;
+            isMixture = false;
+        }
+        else if (isCollection)
+        {
+            CollectionPan.SetActive(false);
+            isCollection = false;
+        }
+    }
     public void ClickMixtureBtn()
     {
-        if(!isMixture)
+        if (!isMixture)
         {
+            AllBtnDown();
             MixturePan.SetActive(true);
+            isQuest = false;
+            isItem = false;
+            isCollection = false;
             isMixture = true;
         }
-        else if(isMixture)
+        else if (isMixture)
         {
             MixturePan.SetActive(false);
             isMixture = false;
         }
+    }
+
+    void AllBtnDown()
+    {
+        QuestPan.SetActive(false);
+        ItemPan.SetActive(false);
+        CollectionPan.SetActive(false);
+        MixturePan.SetActive(false);
     }
 }
