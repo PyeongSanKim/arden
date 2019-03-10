e6fdb7242fbbf167ea468228e6f04b73640e4ade
Cory Kim <corykim@Corys-MacBook-Pro.local>
2019-03-10 23:04:46 +0900

    feat: update number of left function

---
+++ Assets/7.MiniGames/collectRoot/scripts/collectRootManager.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class collectRootManager : MonoBehaviour
+{
+    public int numberOfLeft = 4;
+    public Text numberOfLeftText;
+    // Start is called before the first frame update
+    void Start()
+    {
+        updateNumberOfLeftText();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    void updateNumberOfLeftText(){
+        numberOfLeftText.text = numberOfLeft.ToString();
+    }
+}
