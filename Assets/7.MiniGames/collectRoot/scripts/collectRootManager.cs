1614503f9a4eb165d7be9946d83c9f699bd74788
Cory Kim <corykim@Corys-MacBook-Pro.local>
2019-03-10 23:40:04 +0900

    feat: numbering with random value

--- Assets/7.MiniGames/collectRoot/scripts/collectRootManager.cs
+++ Assets/7.MiniGames/collectRoot/scripts/collectRootManager.cs
@@ -1,16 +1,25 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Serialization;
 using UnityEngine.UI;
 
 public class collectRootManager : MonoBehaviour
 {
-    public int numberOfLeft = 4;
+    [SerializeField]private int numberOfLeft = 4;
+    private int rootValue;
+    private int playerValue;
     public Text numberOfLeftText;
-    // Start is called before the first frame update
+    public Text rootValueText;
+    public Text playerValueText;
+    public Text totalPlayerValueText;
+    private int totalPlayerValue;
+    
     void Start()
     {
         updateNumberOfLeftText();
+        setRootValue();
+        totalPlayerValue = 0;
     }
 
     // Update is called once per frame
@@ -18,8 +27,56 @@ public class collectRootManager : MonoBehaviour
     {
 
     }
+    
+    public void clickCollectBtnWith(string power)
+    {
+        generatePlayerValue(power);
+        totalPlayerValue += playerValue;
+        numberOfLeft--;
+        updateNumberOfLeftText();
+        updatePlayerValue();
+        updateTotalPlayerValue();
+    }
+    
+    private void generatePlayerValue(string power)
+    {
+        switch (power)
+        {
+            case "weak":
+                playerValue = Random.Range(5, 10);
+                break;
+            case "normal":
+                playerValue = Random.Range(13, 23);
+                break;
+            case "strong":
+                playerValue = Random.Range(25, 30);
+                break;
+        }
+    }
 
-    void updateNumberOfLeftText(){
+    void setRootValue()
+    {
+        rootValue = Random.Range(60, 100);
+        updateRootValue();
+    }
+
+    void updateRootValue()
+    {
+        rootValueText.text =  "ROOT\n" + rootValue.ToString();
+    }
+
+    void updatePlayerValue()
+    {
+        playerValueText.text = playerValue.ToString();
+    }
+
+    void updateTotalPlayerValue()
+    {
+        totalPlayerValueText.text = totalPlayerValue.ToString();
+    }
+
+    void updateNumberOfLeftText()
+    {
         numberOfLeftText.text = numberOfLeft.ToString();
     }
 }
